6c7222fa017048e3f3e9a8863e6345cfd8473aa8
Salma <salmafele8@gmail.com>
2019-11-30 23:23:05 -0500

    Initial Commit

---
+++ ScrollView_PageControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollView_PageControl
+//
+//  Created by Salma on 11/30/19.
+//  Copyright © 2019 Salma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
